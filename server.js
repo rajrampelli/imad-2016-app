b286f0c2445cd0f51f9cb85ff1b1ccdaac51e69c
rajrampelli <rampalli.raj@gmail.com>
2016-09-28 19:24:25 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,6 +5,50 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+//Java script object for Article three
+var articleThree = {
+    title: "Article Three | Raj Kumar R",
+    heading: "Article Three",
+    content: `
+        <p>
+		    This is Article One
+		</p>
+		<br>
+		<h1>Story</h1><br>
+		<p>
+		    Article one story
+		</p>
+    `
+
+};
+
+function createTemplate(data) {
+    var title = data.title;
+    var heading = data.heading;
+    var content = data.content;
+var htmlTemple = `
+<html>
+	<head>
+	    <title>
+	        ${title}
+	    </title>
+	    <meta name="viewport" content="width=device-width, initial-scale=1" />
+	    <link href="/ui/style.css" rel="stylesheet" />
+	</head>
+	<body>
+	    <div class="container">
+	        <div>
+	            <a href="/">Home</a>
+	        </div>
+		    <h1>${heading}</h1>
+		    ${content}
+		</div>
+	</body>
+</html>
+`;
+return htmlTemplate;
+}
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
@@ -29,6 +73,10 @@ app.get('/article-two.html', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
 });
 
+//Using Java script object, create html
+app.get('/article-three.html', function (req, res) {
+  res.send(createTemplate(articleThree));
+});
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
